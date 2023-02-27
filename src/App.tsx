4b674c4244a3c0b6579c247ecd7a0b5b41739f8c
filmon-goitom <philipmilio9@gmail.com>
2023-02-27 14:08:06 +0100

    adding contac as

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import {About}  from './pages/About';
+import {ContactAs}  from './pages/ContactAs';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -37,6 +38,9 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/about">
           <About />
+        </Route>
+        <Route exact path="/contactas">
+          <ContactAs />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
